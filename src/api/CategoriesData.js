3f40b7366dd14ef522a1f1f67f520dca7e55bfe9
mgdeveloper1406 <mg.developer@yahoo.com>
2024-01-26 15:27:01 -0300

    edicion

--- src/api/CategoriesData.js
+++ src/api/CategoriesData.js
@@ -1,25 +1,25 @@
 const CategoriesData = [
   {
     id: '1',
-    categoryName: 'Breakfasts',
+    categoryName: 'Breakfast',
     cardColor: 'grey',
     icon: 'https://static.vecteezy.com/system/resources/previews/022/660/505/non_2x/bread-with-berries-3d-breakfast-icon-png.png'
   },
   {
     id: '2',
-    categoryName: 'Healthy',
+    categoryName: 'Bread',
     cardColor: 'green',
     icon: 'https://static.vecteezy.com/system/resources/previews/028/567/867/original/salad-3d-rendering-icon-illustration-free-png.png'
   },
   {
     id: '3',
-    categoryName: 'Main',
+    categoryName: 'Meat',
     cardColor: 'red',
     icon: 'https://static.vecteezy.com/system/resources/previews/011/027/526/original/korean-icon-bibimbap-food-3d-illustration-png.png'
   },
   {
     id: '4',
-    categoryName: 'Drinks',
+    categoryName: 'Dessert',
     cardColor: 'yellow',
     icon: 'https://static.vecteezy.com/system/resources/previews/008/484/761/original/3d-illustration-object-icon-drink-cup-can-be-used-for-web-app-info-graphic-etc-png.png'
   },
